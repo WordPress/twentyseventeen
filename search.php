74956f7d54bab5a87f28bdf6341b3add5216345a
laurelfulford <laurelfulford@gmail.com>
2016-09-10 10:52:38 -0700

    Revert "Merge branch 'juanfra-master'"
    This reverts commit 81b848611250c5256544740c3a9fca4e6c2fef55, reversing
    changes made to 69482880b4ae2ec145f60df417423665581a5653.
    
    Reverting the last commit; I made an error, and the escaping seems unneeded.

--- search.php
+++ search.php
@@ -16,7 +16,7 @@ get_header(); ?>
 		if ( have_posts() ) : ?>
 
 			<header class="page-header">
-				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyseventeen' ), '<span>' . esc_html( get_search_query() ) . '</span>' ); ?></h1>
+				<h1 class="page-title"><?php printf( __( 'Search Results for: %s', 'twentyseventeen' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
 			</header>
 			<?php
 			/* Start the Loop */
