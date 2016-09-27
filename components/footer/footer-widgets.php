d15644a4dd7181e31b8951c641cfbf7fd6df924f
Bappi <bappi.cs@gmail.com>
2016-09-27 12:56:51 +0600

    Add closing div
    Line 31

--- components/footer/footer-widgets.php
+++ components/footer/footer-widgets.php
@@ -28,6 +28,7 @@ if ( is_active_sidebar( 'sidebar-2' ) ||
 			if ( is_active_sidebar( 'sidebar-4' ) ) { ?>
 				<div class="widget-column">
 					<?php dynamic_sidebar( 'sidebar-4' ); ?>
+				</div>
 			<?php } ?>
 		</div><!-- .wrap -->
 	</aside><!-- .widget-area -->
