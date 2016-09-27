2b2f72383080d9e869de461a985c79e77466bc7d
laurelfulford <laurelfulford@gmail.com>
2016-09-27 16:32:54 -0700

    Centre featured image on single posts and pages. See #4.

--- header.php
+++ header.php
@@ -41,7 +41,7 @@
 	<?php
 	// If a regular post or page, and not the front page, show the featured image
 	if ( has_post_thumbnail() && ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) ) :
-		echo '<div class="">';
+		echo '<div class="single-featured-image-header">';
 		the_post_thumbnail( 'twentyseventeen-featured-image' );
 		echo '</div>';
 	endif;
