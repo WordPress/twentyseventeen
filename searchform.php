df8f1ee6c3e79310676532afd25c3a60a4faae2e
laurelfulford <laurelfulford@gmail.com>
2016-09-27 17:19:52 -0700

    Widget styles for Twenty Seventeen: * Update search markup and styles to match mockups * Add the same hover transition to tag clouds as other links * Remove underlines from links in categories, archives, etc. widgets. Paired with the borders dividing the links, it's a bit much. * Adjust spacing on top and bottom of nested lists * Decrease font size overall
    See #4.

---
+++ searchform.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Template for displaying search forms in Twenty Seventeen
+ *
+ * @package Twenty Seventeen
+ */
+?>
+
+<form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
+	<label>
+		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'twentyseventeen' ); ?></span>
+		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyseventeen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" title="<?php echo esc_attr_x( 'Search for:', 'label', 'twentyseventeen' ); ?>" />
+	</label>
+	<button type="submit" class="search-submit"><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'twentyseventeen' ); ?></span></button>
+</form>
