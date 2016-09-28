f646bb01bae7acdb61c03e57e549a7c062c78afa
laurelfulford <laurelfulford@gmail.com>
2016-09-28 09:23:13 -0700

    Minor updates to front page: * Remove sticky posts from recent posts panel * Update 'Continue reading' string to remove arrow.
    See #4.

--- components/page/content-front-page-panels.php
+++ components/page/content-front-page-panels.php
@@ -44,7 +44,7 @@ global $twentyseventeencounter;
 				<?php
 					/* translators: %s: Name of current post */
 					the_content( sprintf(
-						__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyseventeen' ),
+						__( 'Continue reading %s', 'twentyseventeen' ),
 						the_title( '<span class="screen-reader-text">"', '"</span>', false )
 					) );
 				?>
@@ -56,8 +56,9 @@ global $twentyseventeencounter;
 
 				<?php // Show four most recent posts
 				$recent_posts = new WP_Query( array(
-					'posts_per_page' => 3,
-					'post_status'    => 'publish',
+					'posts_per_page'      => 3,
+					'post_status'         => 'publish',
+					'ignore_sticky_posts' => true
 				) );
 				?>
 
--- components/page/content-front-page.php
+++ components/page/content-front-page.php
@@ -13,7 +13,7 @@
 				<?php
 					/* translators: %s: Name of current post */
 					the_content( sprintf(
-						__( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'twentyseventeen' ),
+						__( 'Continue reading %s', 'twentyseventeen' ),
 						the_title( '<span class="screen-reader-text">"', '"</span>', false )
 					) );
 				?>
