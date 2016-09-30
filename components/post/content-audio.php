25cf90703073172aa94161b729042258776b437f
laurelfulford <laurelfulford@gmail.com>
2016-09-30 14:13:43 -0700

    Build out theme's post format support: * Update audio, gallery, video and image post formats, so they only show the media on the blog index page. * Adding fallback for audio, video, gallery and image post formats. If each doesn't have its featured media in the content, it will show the regular content on the blog index instead. * Remove support for Status and Chat post formats.
    See #4.

---
+++ components/post/content-audio.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Template part for displaying audio posts.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Twenty Seventeen
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php
+			if ( 'post' === get_post_type() ) :
+				echo '<div class="entry-meta">';
+					if ( is_single() ) :
+						twentyseventeen_posted_on();
+					else :
+						echo twentyseventeen_time_link();
+					endif;
+				echo '</div><!-- .entry-meta -->';
+			endif;
+
+			if ( is_single() ) {
+				the_title( '<h1 class="entry-title">', '</h1>' );
+			} else {
+				the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+			}
+		?>
+	</header><!-- .entry-header -->
+
+	<?php if ( '' !== get_the_post_thumbnail() ) : ?>
+		<div class="post-thumbnail">
+			<a href="<?php the_permalink(); ?>">
+				<?php the_post_thumbnail( 'twentyseventeen-featured-image' ); ?>
+			</a>
+		</div><!-- .post-thumbnail -->
+	<?php endif; ?>
+
+	<div class="entry-content">
+
+		<?php if ( ! is_single() ) :
+
+			// If not a single post, highlight the audio file
+
+			$content = apply_filters( 'the_content', get_the_content() );
+			$audio = get_media_embedded_in_content( $content, array( 'audio' ) );
+
+			if ( ! empty( $audio ) ) :
+				foreach ( $audio as $audio_html ) {
+					echo '<div class="entry-audio">';
+						echo $audio_html;
+					echo '</div><!-- .entry-audio -->';
+				}
+			endif;
+
+		endif;
+
+		if ( is_single() || empty( $audio ) ) :
+
+			the_content( sprintf(
+				/* translators: %s: Name of current post. */
+				__( 'Continue reading %s', 'twentyseventeen' ),
+				the_title( '<span class="screen-reader-text">"', '"</span>', false )
+			) );
+
+			wp_link_pages( array(
+				'before'      => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
+				'after'       => '</div>',
+				'link_before' => '<span class="page-number">',
+				'link_after'  => '</span>',
+			) );
+
+		endif; ?>
+
+	</div><!-- .entry-content -->
+
+</article><!-- #post-## -->
