de75c080a43348cad9ee6e199dfaff442b82da46
Mészáros Róbert <hmm@meszarosrob.com>
2016-09-30 18:58:37 +0300

    Remove example code. Fixes #28

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -10,18 +10,6 @@
  */
 
 /**
- * Sample implementation of the Custom Header feature
- *
- *  You can add an optional custom header image to header.php like so ...
-
-	<?php if ( get_header_image() ) : ?>
-	<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
-		<img src="<?php header_image(); ?>" width="<?php echo esc_attr( get_custom_header()->width ); ?>" height="<?php echo esc_attr( get_custom_header()->height ); ?>" alt="">
-	</a>
-	<?php endif; // End header image check. ?>
- */
-
-/**
  * Set up the WordPress core custom header feature.
  *
  * @uses twentyseventeen_header_style()
