e6ffb42e0569ced119d9ef95193fde352661cdc5
laurelfulford <laurelfulford@gmail.com>
2016-10-10 14:52:29 -0700

    Fixing Travis build error; adding comma after all lines in an array. See #241.

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -28,7 +28,7 @@ function twentyseventeen_custom_header_setup() {
 		'default-image' => array(
 			'url'           => '%s/assets/images/header.jpg',
 			'thumbnail_url' => '%s/assets/images/header.jpg',
-			'description'   => __( 'Default Header Image', 'twentyseventeen' )
+			'description'   => __( 'Default Header Image', 'twentyseventeen' ),
 		),
 	) );
 }
