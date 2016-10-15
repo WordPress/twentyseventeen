2d0d7737061811d357be58ee955eeacfadd42534
David A. Kennedy <me@davidakennedy.com>
2016-10-15 15:08:32 -0400

    Add unique `id` to custom search form.
    * This provides more robust support among assistive technology than just nesting the input in the label.
    
    See #336.

--- searchform.php
+++ searchform.php
@@ -9,10 +9,12 @@
  */
 ?>
 
+<?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
+
 <form role="search" method="get" class="search-form" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label>
+	<label for="<?php echo $unique_id; ?>">
 		<span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'twentyseventeen' ); ?></span>
-		<input type="search" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyseventeen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
+		<input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'twentyseventeen' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
 	</label>
 	<button type="submit" class="search-submit"><?php echo twentyseventeen_get_svg( array( 'icon' => 'search' ) ); ?><span class="screen-reader-text"><?php echo _x( 'Search', 'submit button', 'twentyseventeen' ); ?></span></button>
 </form>
