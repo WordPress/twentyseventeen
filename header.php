1cc3be0c87b74374e881c7114049a9f9c9b2384c
enodekciw <tomas@dekciw.lt>
2016-10-18 14:40:49 +0300

    prevents navigation menu from showing up if no menu is assigned

--- header.php
+++ header.php
@@ -30,11 +30,13 @@
 
 		<?php get_template_part( 'components/header/header', 'image' ); ?>
 
-		<div class="navigation-top">
-			<div class="wrap">
-				<?php get_template_part( 'components/navigation/navigation', 'top' ); ?>
-			</div><!-- .wrap -->
-		</div><!-- .navigation-top -->
+		<?php if( has_nav_menu( 'top' ) ) : ?>
+			<div class="navigation-top">
+				<div class="wrap">
+					<?php get_template_part( 'components/navigation/navigation', 'top' ); ?>
+				</div><!-- .wrap -->
+			</div><!-- .navigation-top -->
+		<?php endif; ?>
 
 	</header><!-- #masthead -->
 
