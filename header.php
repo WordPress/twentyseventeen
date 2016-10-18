f737b741e0c729337981655e9435b4a03c1602ee
enodekciw <tomas@dekciw.lt>
2016-10-18 14:56:41 +0300

    should fix travis-ci build

--- header.php
+++ header.php
@@ -30,7 +30,7 @@
 
 		<?php get_template_part( 'components/header/header', 'image' ); ?>
 
-		<?php if( has_nav_menu( 'top' ) ) : ?>
+		<?php if ( has_nav_menu( 'top' ) ) : ?>
 			<div class="navigation-top">
 				<div class="wrap">
 					<?php get_template_part( 'components/navigation/navigation', 'top' ); ?>
