cbb30e5b87f227daa88026a61c11613fa7d2aea7
laurelfulford <laurelfulford@gmail.com>
2016-10-18 17:06:19 -0700

    Replacing dynamic header for blog posts page with static one. Fixes #437.

--- index.php
+++ index.php
@@ -18,16 +18,10 @@
 get_header(); ?>
 
 <div class="wrap">
-	<?php
-	if ( is_home() && ! is_front_page() ) : ?>
-		<header class="page-header">
-			<h1 class="page-title"><?php single_post_title(); ?></h1>
-		</header>
-	<?php elseif ( is_home() ) : ?>
-		<header class="page-header">
-			<h2 class="page-title"><?php echo get_the_title( get_option( 'page_for_posts' ) ); ?></h2>
-		</header>
-	<?php endif; ?>
+	<header class="page-header">
+		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
+	</header>
+
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
