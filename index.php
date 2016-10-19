24231443b2b5ce609d12bb4971534b12a10c0ce2
David A. Kennedy <me@davidakennedy.com>
2016-10-18 22:09:04 -0400

    Replacing dynamic header for blog posts page with static one.
    * Front page with posts says "Posts".
    * Static front page shows page title.
    
    Fixes #437.

--- index.php
+++ index.php
@@ -18,9 +18,15 @@
 get_header(); ?>
 
 <div class="wrap">
+	<?php if ( is_home() && ! is_front_page() ) : ?>
+		<header class="page-header">
+			<h1 class="page-title"><?php single_post_title(); ?></h1>
+		</header>
+	<?php else : ?>
 	<header class="page-header">
 		<h2 class="page-title"><?php _e( 'Posts', 'twentyseventeen' ); ?></h2>
 	</header>
+	<?php endif; ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
